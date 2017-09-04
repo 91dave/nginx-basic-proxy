4aa12a97c99466876f758d123b44370eab04f3c0
root <root@ip-172-31-29-155.ec2.internal>
2017-09-04 12:00:26 +0000

    Implement elastic-search logging using ES_LOGS environment vars for config

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
 FROM nginx:alpine
 
+# Global bits that never change
+WORKDIR /app/
+ENTRYPOINT [ "./app.sh" ]
+
+# Dependencies that should rarely change
+RUN apk update && apk add curl
+
+# Application bits that change most often
 COPY app.sh /app/
+COPY json.conf /app/
 COPY setup-site.sh /app/
 COPY setup-redirect.sh /app/
-WORKDIR /app/
 
-ENTRYPOINT [ "./app.sh" ]
--- app.sh
+++ app.sh
@@ -31,5 +31,30 @@ do
 
 done
 
-nginx -g 'daemon off;'
+
+use_es=false
+[ -n "$ES_LOG_URL" ] && [ -n "$ES_LOG_INDEX" ] && use_es=true
+
+if [ "$use_es" = "true" ]
+then
+
+	cp json.conf /etc/nginx/conf.d
+    echo "Using Elastic Search logging, writing logs to $ES_LOG_URL/$ES_LOG_INDEX"
+
+	nginx -g 'daemon off;' | while read log
+	do
+		
+        #timestamp=$(date +%Y-%m-%dT%H:%M:%S.000000000Z)
+		index=$(date $ES_LOG_INDEX)
+        entry=$(echo $log | sed -e "s/EXTRAPROPS/$ES_LOG_PROPS/g" )
+
+        echo $entry | curl -X POST $ES_LOG_URL/$index/logs -d @- -H "Content-Type: application/json" -s
+        echo ""
+        echo $entry
+	done
+
+else
+	nginx -g 'daemon off;'
+fi
+
 
